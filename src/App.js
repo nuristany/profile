3262437132d51a668e8760c68fdddc4638abc261
Zhakya Nuristany <89075691+nuristany@users.noreply.github.com>
2024-08-03 10:42:54 +0430

    added styles for mobile devices

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Project from "./components/Project";
 import "./styles.css"
+import "./media.css"
 
 
 function App() {
