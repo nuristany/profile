3b343ff6147ea5fbac115627e268ca7cf5ae9da3
Zhakya Nuristany <89075691+nuristany@users.noreply.github.com>
2024-08-06 09:25:44 +0430

    changed max-width to 1200px in media.css

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import limo from "../images/Screenshot 2024-08-05 162053.png";
+import rating from "../images/Screenshot 2024-08-05 170015.png";
 
 const Project = () => {
-  return <div>projects</div>;
+  return (
+    <div className="project-container">
+      <div className="project-detail">
+        <img src={limo} alt="" />
+        <div className="project-title">
+          <h1>Project One</h1>
+        </div>
+        <div className="project-button">
+          <button>GitHub</button>
+          <button>Live Demo</button>
+        </div>
+      </div>
+      <div className="project-detail">
+        <img src={rating} alt="" />
+        <div className="project-title">
+          <h1>Project Two</h1>
+        </div>
+        <div className="project-button">
+          <button>GitHub</button>
+          <button>Live Demo</button>
+        </div>
+      </div>
+      <div className="project-detail">
+        <img src={limo} alt="" />
+        <div className="project-title">
+          <h1>Project Three</h1>
+        </div>
+        <div className="project-button">
+          <button>GitHub</button>
+          <button>Live Demo</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
