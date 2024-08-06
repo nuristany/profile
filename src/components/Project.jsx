2128bf14e7bc4ab13c712f0e3b974e25cb641161
Zhakya Nuristany <89075691+nuristany@users.noreply.github.com>
2024-08-06 12:19:32 +0430

    changed max-width to 1200 & 500 in media.css

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,34 +4,40 @@ import rating from "../images/Screenshot 2024-08-05 170015.png";
 const Project = () => {
   return (
     <div className="project-container">
-      <div className="project-detail">
-        <img src={limo} alt="" />
-        <div className="project-title">
-          <h1>Project One</h1>
-        </div>
-        <div className="project-button">
-          <button>GitHub</button>
-          <button>Live Demo</button>
-        </div>
+      <div className="project-title-container">
+        <p>Browse My Recent Projects</p>
+        <h1 style={{color:"black", marginTop:"5px"}}>Projects</h1>
       </div>
-      <div className="project-detail">
-        <img src={rating} alt="" />
-        <div className="project-title">
-          <h1>Project Two</h1>
+      <div className="project-content-container">
+        <div className="project-detail">
+          <img src={limo} alt="" />
+          <div className="project-title">
+            <h1>Project One</h1>
+          </div>
+          <div className="project-button">
+            <button>GitHub</button>
+            <button>Live Demo</button>
+          </div>
         </div>
-        <div className="project-button">
-          <button>GitHub</button>
-          <button>Live Demo</button>
+        <div className="project-detail">
+          <img src={rating} alt="" />
+          <div className="project-title">
+            <h1>Project Two</h1>
+          </div>
+          <div className="project-button">
+            <button>GitHub</button>
+            <button>Live Demo</button>
+          </div>
         </div>
-      </div>
-      <div className="project-detail">
-        <img src={limo} alt="" />
-        <div className="project-title">
-          <h1>Project Three</h1>
-        </div>
-        <div className="project-button">
-          <button>GitHub</button>
-          <button>Live Demo</button>
+        <div className="project-detail">
+          <img src={limo} alt="" />
+          <div className="project-title">
+            <h1>Project Three</h1>
+          </div>
+          <div className="project-button">
+            <button>GitHub</button>
+            <button>Live Demo</button>
+          </div>
         </div>
       </div>
     </div>
